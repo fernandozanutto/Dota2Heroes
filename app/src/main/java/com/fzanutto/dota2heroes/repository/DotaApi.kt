0c3cf25c7a83115e35352b9bc6edd9e1e7f0cdef
Fernando Zanutto <ferzanutto1999@gmail.com>
2022-06-12 16:44:04 -0300

    Retrofit

--- app/src/main/java/com/fzanutto/dota2heroes/repository/DotaApi.kt
+++ app/src/main/java/com/fzanutto/dota2heroes/repository/DotaApi.kt
@@ -1,4 +1,12 @@
 package com.fzanutto.dota2heroes.repository
 
-class DotaApi {
+import com.fzanutto.dota2heroes.model.Hero
+import retrofit2.http.GET
+
+interface DotaApi: ApiConnection {
+
+    val baseUrl = "https://api.opendota.com"
+
+    @GET("api/heroStats")
+    override suspend fun getHeroesList(): List<Hero>
 }
